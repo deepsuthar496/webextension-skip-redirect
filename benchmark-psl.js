3b1c600d88361330213b1929a8be108c01cc05a5
Deepsuthar496 <151819823+deepsuthar496@users.noreply.github.com>
2024-03-29 19:51:37 +0530

    improve

--- benchmark-psl.js
+++ benchmark-psl.js
@@ -1,16 +1,23 @@
 const test = require("tape-benchmark")(require("tape"));
-
 const psl = require("./psl");
 const pslrules = require("./pslrules");
 
 test("benchmark", function(test) {
-    test.benchmark("Search through rules", function() {
-        for (const entry of pslrules.NORMAL_ENTRIES) {
-            "foobar.foo.bar.ck".endsWith(entry);
-        }
-    });
-    test.benchmark("Set implementation", function() {
-        psl.getDomain("foobar.foo.bar.ck");
-    });
-    test.end();
+  test.benchmark("Search through rules", function() {
+    for (const entry of pslrules.NORMAL_ENTRIES) {
+      if (typeof entry === "string") {
+        entry.endsWith("foobar.foo.bar.ck");
+      }
+    }
+  });
+
+  test.benchmark("Set implementation", function() {
+    try {
+      psl.getDomain("foobar.foo.bar.ck");
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  test.end();
 });
