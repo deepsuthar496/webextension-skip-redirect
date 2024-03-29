7fb5f734d51dfb6ac5ee8c36b929aba4007cccb5
Deepsuthar496 <151819823+deepsuthar496@users.noreply.github.com>
2024-03-29 19:51:43 +0530

    improve

--- util.js
+++ util.js
@@ -1,22 +1,21 @@
-const util = (function(root) { //  eslint-disable-line no-unused-vars
+const util = (() => {
+  'use strict';
 
-    function mergeList(target, source) {
-        if (target == undefined) {
-            return source == undefined ? [] : source;
-        }
-        if (source == undefined) {
-            return target;
-        }
-        return [
-            ...target,
-            ...source.filter((element) => ! target.includes(element)),
-        ];
+  function mergeList(target, source) {
+    if (target === undefined) {
+      return source === undefined ? [] : source;
     }
+    if (source === undefined) {
+      return target;
+    }
+    return [
+      ...target,
+      ...source.filter((element) => !target.includes(element)),
+    ];
+  }
 
-    root.mergeList = mergeList;
-
-    return {
-        mergeList,
-    };
-
-})(this);
+  // Module exports
+  return {
+    mergeList,
+  };
+})();
